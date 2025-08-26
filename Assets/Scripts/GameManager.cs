f916fbcf89b8a215e782f7e8c4a5793471123874
Jannes <37171@ma-web.nl>
2025-08-26 20:48:23 +0200

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 public class GameManager : MonoBehaviour
 {
 	private float LapsedTime = 0f;
 
-	[SerializeField] private float GameSpeed = 1f;
+	[FormerlySerializedAs("GameSpeed")] [SerializeField] private float gameSpeed = 1f;
 
 	[SerializeField] private List<LiveCell> liveCells = new List<LiveCell>();
 
+	[SerializeField] private List<LiveCell> testPointToCheck = new List<LiveCell>();
+
 	// Start is called before the first frame update
 	void Start()
-	{
+	{			
+		UpdateGameState();
 		/*LiveCell zero = new LiveCell(0,0);
-		/*print(GetNeighborCount(liveCells[2]));
+		print(GetNeighborCount(liveCells[2]));
 		for (int i = 0; i < 5; i++)
 		{
 			for (int j = 0; j < 5; j++)
@@ -29,7 +33,7 @@ public class GameManager : MonoBehaviour
 	void Updatee()
 	{
 		LapsedTime += Time.deltaTime;
-		if (LapsedTime % GameSpeed <= 0.001f)
+		if (LapsedTime % gameSpeed <= 0.001f)
 		{
 			print("tick");
 			print(liveCells);
@@ -39,29 +43,29 @@ public class GameManager : MonoBehaviour
 
 	List<LiveCell> GetCellsToCheck(List<LiveCell> liveCells)
 	{
-		List<LiveCell> CellsToCheck = new List<LiveCell>();
+		List<LiveCell> cellsToCheck = new List<LiveCell>();
 
 		foreach (var currentCell in liveCells)
 		{
 			// yank af, but works for now
 			// y  1
-			CellsToCheck.Add(new LiveCell(currentCell.x + 1, currentCell.y + 1));
-			CellsToCheck.Add(new LiveCell(currentCell.x, currentCell.y + 1));
-			CellsToCheck.Add(new LiveCell(currentCell.x - 1, currentCell.y + 1));
+			cellsToCheck.Add(new LiveCell(currentCell.x + 1, currentCell.y + 1));
+			cellsToCheck.Add(new LiveCell(currentCell.x, currentCell.y + 1));
+			cellsToCheck.Add(new LiveCell(currentCell.x - 1, currentCell.y + 1));
 
 			// y  0 
-			CellsToCheck.Add(new LiveCell(currentCell.x + 1, currentCell.y));
-			CellsToCheck.Add(new LiveCell(currentCell.x, currentCell.y));
-			CellsToCheck.Add(new LiveCell(currentCell.x - 1, currentCell.y));
+			cellsToCheck.Add(new LiveCell(currentCell.x + 1, currentCell.y));
+			cellsToCheck.Add(new LiveCell(currentCell.x, currentCell.y));
+			cellsToCheck.Add(new LiveCell(currentCell.x - 1, currentCell.y));
 
 			// y -1
-			CellsToCheck.Add(new LiveCell(currentCell.x + 1, currentCell.y - 1));
-			CellsToCheck.Add(new LiveCell(currentCell.x, currentCell.y - 1));
-			CellsToCheck.Add(new LiveCell(currentCell.x - 1, currentCell.y - 1));
+			cellsToCheck.Add(new LiveCell(currentCell.x + 1, currentCell.y - 1));
+			cellsToCheck.Add(new LiveCell(currentCell.x, currentCell.y - 1));
+			cellsToCheck.Add(new LiveCell(currentCell.x - 1, currentCell.y - 1));
 		}
 				
 		// remove duplicates and return
-		return CellsToCheck.Distinct().ToList();
+		return cellsToCheck.Distinct().ToList();
 	}
 	
 	bool AreCellsNeighbours(LiveCell cellOne, LiveCell cellTwo)
@@ -85,6 +89,7 @@ public class GameManager : MonoBehaviour
 		foreach (var newCell in cellsToCheck)
 		{
 			int neighborCount = GetNeighborCount(newCell);
+			print($"{neighborCount} neighbors found, {newCell.x}, {newCell.y}");
 			if (IsCellAlive(newCell))
 			{
 				if (neighborCount == 3 || neighborCount == 2)
@@ -106,10 +111,14 @@ public class GameManager : MonoBehaviour
 	void UpdateGameState()
 	{
 		// make list of positions that have at least one live cell neighboring,
-		List<LiveCell> CellsToCheck = GetCellsToCheck(liveCells);
+		List<LiveCell> cellsToCheck = GetCellsToCheck(liveCells);
+		foreach (var test in cellsToCheck)
+		{
+			print($"test: {test.x}, {test.y}");
+		}
 		
 		// make list of new game state
-		liveCells = GetNewGameState(CellsToCheck);
+		liveCells = GetNewGameState(cellsToCheck);
 	}
 }
 
